6c44e8602529fc81efb04cf7b967c510dca10aa4
JerrryLu <jclu2gm@gmail.com>
2023-03-11 20:10:53 -0800

    finished button count

---
+++ js/ButtonCount.js
@@ -0,0 +1,24 @@
+class ButtonCount extends HTMLElement {
+  constructor() {
+    super();
+
+    let shadow = this.attachShadow({ mode: "open" });
+
+    this.button_clicks = 0;
+    let button_element = document.createElement("button");
+    button_element.innerHTML = "Times Clicked: 0";
+    let click_increment = () => {
+      this.#click_function(button_element);
+    };
+    button_element.addEventListener('click', click_increment);
+
+    shadow.appendChild(button_element);
+  }
+
+  #click_function(button_element) {
+    this.button_clicks++;
+    button_element.innerHTML = "Times Clicked: " + this.button_clicks;
+  }
+}
+
+customElements.define("button-count", ButtonCount);
